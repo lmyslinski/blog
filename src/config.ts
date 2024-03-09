c37d425ae21a4fc15b28f764025c6d225e0640c4
Łukasz Myśliński <lukasz@mlnative.com>
2024-03-09 17:44:41 +0100

    More posts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "lmyslinski.com",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 6,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
