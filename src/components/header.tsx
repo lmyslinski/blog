d75f2c57cef1ee10b2f74640b380776a3d5d9ca3
Łukasz Myśliński <lukasz@mlnative.com>
2024-03-20 23:45:45 +0100

    responsive tweaks

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,9 @@
 import { ModeToggle } from "./mode-toggle";
-import { Separator } from "./ui/separator";
 
 export default function Header() {
   return (
     <div className="w-full border-b py-2 flex justify-center">
-      <div className="flex justify-between w-full max-w-7xl px-8">
+      <div className="flex justify-between w-full max-w-7xl px-4">
         <div className="flex items-center">
           <a href="/">
             <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
@@ -12,14 +11,13 @@ export default function Header() {
             </h3>
           </a>
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-2 md:space-x-4">
           <a className="text-muted-foreground" href="/">
             Home
           </a>
           <a className="text-muted-foreground" href="/blog">
             Articles
           </a>
-          <Separator orientation="vertical" />
           <ModeToggle />
         </div>
       </div>
